a14ad3c9d49cf055b2d280b8cbb400b76757b8e3
Ifrazahoor <ifra8766@gmail.com>
2024-12-08 21:24:14 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+import Hero from "@/components/Hero"
+import Carames from "@/components/Carames"
+import Brand from "@/components/Brand"
+import Product from  "@/components/Product"
+import Benefit from "@/components/Benefit"
+import Touch from "@/components/Touch"
+
+export default function(){
+  return(
+    <>
+    
+    <Hero />
+    <Brand />
+<Carames />
+<Product />
+<Benefit />
+   <Touch />
+  
+    </>
+  )
+}
